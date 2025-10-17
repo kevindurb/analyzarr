b529b069e4d7e58c6304435a799fb8c18ec984eb
Kevin Durbin <kevindurb@fastmail.com>
2025-10-17 09:16:47 -0600

    clean out uneeded things

--- src/infrastructure/ffprobe.ts
+++ src/infrastructure/ffprobe.ts
@@ -1,10 +1,5 @@
 import { $ } from 'bun';
 import z from 'zod';
-import { stringToFloat, stringToInt } from '@/util/zod';
-
-export const FFProbeEmptyStream = z.object({
-  codec_type: z.literal('').optional(),
-});
 
 export const FFProbeSubtitleStream = z.object({
   index: z.number(),
@@ -17,44 +12,25 @@ export const FFProbeVideoStream = z.object({
   codec_type: z.literal('video'),
   width: z.int(),
   height: z.int(),
-  duration: stringToFloat.optional(),
-  bit_rate: stringToInt.optional(),
 });
 
 export const FFProbeAudioStream = z.object({
   index: z.number(),
   codec_name: z.string(),
   codec_type: z.literal('audio'),
-  duration: stringToFloat,
-  bit_rate: stringToInt,
-  channels: z.number(),
 });
 
 export const FFProbeDataStream = z.object({
   index: z.number(),
   codec_type: z.literal('data'),
-  duration: stringToFloat,
 });
 
-export const FFProbeFormat = z.object({
-  filename: z.string(),
-  format_name: z.string(),
-  format_long_name: z.string(),
-  duration: stringToFloat.optional(),
-  size: stringToInt,
-  bit_rate: stringToInt.optional(),
-});
+export const FFProbeFormat = z.object({});
 
 export const FFProbeOutput = z.object({
   streams: z
     .array(
-      z.union([
-        FFProbeVideoStream,
-        FFProbeAudioStream,
-        FFProbeDataStream,
-        FFProbeSubtitleStream,
-        FFProbeEmptyStream,
-      ]),
+      z.union([FFProbeVideoStream, FFProbeAudioStream, FFProbeDataStream, FFProbeSubtitleStream]),
     )
     .optional(),
   format: FFProbeFormat.optional(),
