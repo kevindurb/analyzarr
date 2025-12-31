b7e48d6f5cdcf946b075d3ae19fb380201022b16
Kevin Durbin <kevindurb@fastmail.com>
2025-12-31 16:06:24 -0700

    remove worker api

--- src/app/jobs/libraryProber.ts
+++ src/app/jobs/libraryProber.ts
@@ -1,9 +1,6 @@
 import { Cron } from 'croner';
-import type { File } from '@/generated/prisma';
 import { ffprobe } from '@/infrastructure/ffprobe';
 import { prisma } from '@/infrastructure/prisma';
-
-declare var self: Worker;
 
 export const run = async () => {
   const files = await prisma.file.findMany({
@@ -11,51 +8,38 @@ export const run = async () => {
     take: 100,
   });
   console.log('Starting Library Prober with files', files.length);
-  const worker = new Worker(import.meta.url);
-  worker.postMessage({ files });
-  worker.addEventListener('error', (error) => console.error('Library Prober', error));
+
+  const updates = [];
+
+  for (const { id, filePath } of files) {
+    try {
+      const data = await ffprobe(filePath);
+      if (!data) continue;
+      const videoStream = data.streams?.find((stream) => stream.codec_type === 'video');
+      const audioStream = data.streams?.find((stream) => stream.codec_type === 'audio');
+
+      updates.push(
+        prisma.file.update({
+          where: {
+            id,
+          },
+          data: {
+            videoCodec: videoStream?.codec_name,
+            videoHeight: videoStream?.height,
+            videoWidth: videoStream?.width,
+            audioCodec: audioStream?.codec_name,
+            disableProbe: !data.format,
+          },
+        }),
+      );
+    } catch (err) {
+      console.error('Error probing file', filePath, err);
+    }
+  }
+
+  await Promise.all(updates);
+
+  console.log('Done scanning');
 };
 
 export const startSchedule = () => new Cron('15 * * * *', run);
-
-self.onmessage = async (event: MessageEvent<{ files: File[] }>) => {
-  try {
-    const { files } = event.data;
-    console.log(`Probing ${files.length} files`);
-
-    const updates = [];
-
-    for (const { id, filePath } of files) {
-      try {
-        const data = await ffprobe(filePath);
-        if (!data) continue;
-        const videoStream = data.streams?.find((stream) => stream.codec_type === 'video');
-        const audioStream = data.streams?.find((stream) => stream.codec_type === 'audio');
-
-        updates.push(
-          prisma.file.update({
-            where: {
-              id,
-            },
-            data: {
-              videoCodec: videoStream?.codec_name,
-              videoHeight: videoStream?.height,
-              videoWidth: videoStream?.width,
-              audioCodec: audioStream?.codec_name,
-              disableProbe: !data.format,
-            },
-          }),
-        );
-      } catch (err) {
-        console.error('Error probing file', filePath, err);
-      }
-    }
-
-    await Promise.all(updates);
-    await prisma.$disconnect();
-
-    console.log('Done scanning');
-  } finally {
-    process.exit();
-  }
-};
--- src/app/jobs/libraryScanner.ts
+++ src/app/jobs/libraryScanner.ts
@@ -3,13 +3,37 @@ import { type File, getAllFilesInDir } from '@/infrastructure/filesService';
 import { prisma } from '@/infrastructure/prisma';
 import * as libraryProber from './libraryProber';
 
-declare var self: Worker;
-
-export const run = (libraryId: string) => {
+export const run = async (libraryId: string) => {
   console.log('Starting Library Scanner');
-  const worker = new Worker(import.meta.url);
-  worker.postMessage({ libraryId });
-  worker.addEventListener('error', (error) => console.error('Library Scanner', error));
+  const library = await prisma.library.findUniqueOrThrow({
+    where: { id: libraryId },
+    include: { files: true },
+  });
+
+  const foundPaths: string[] = [];
+  const filesToCreate: File[] = [];
+
+  for await (const file of getAllFilesInDir(library.path)) {
+    console.log('Found File', file);
+    foundPaths.push(file.filePath);
+    const exists = library.files.find(({ filePath }) => file.filePath === filePath);
+    if (!exists) filesToCreate.push(file);
+  }
+
+  await prisma.file.createMany({
+    data: filesToCreate.map((file) => ({
+      ...file,
+      fileSize: Math.floor(file.fileSize),
+      libraryId: library.id,
+    })),
+  });
+
+  await prisma.file.deleteMany({
+    where: { filePath: { notIn: foundPaths }, libraryId: library.id },
+  });
+  console.log('Done scanning');
+
+  libraryProber.run();
 };
 
 const scanAllLibraries = async () => {
@@ -20,42 +44,3 @@ const scanAllLibraries = async () => {
 };
 
 export const startSchedule = () => new Cron('0 * * * *', scanAllLibraries);
-
-self.onmessage = async (event: MessageEvent<{ libraryId: string }>) => {
-  try {
-    console.log('Starting');
-    const library = await prisma.library.findUniqueOrThrow({
-      where: { id: event.data.libraryId },
-      include: { files: true },
-    });
-
-    const foundPaths: string[] = [];
-    const filesToCreate: File[] = [];
-
-    for await (const file of getAllFilesInDir(library.path)) {
-      console.log('Found File', file);
-      foundPaths.push(file.filePath);
-      const exists = library.files.find(({ filePath }) => file.filePath === filePath);
-      if (!exists) filesToCreate.push(file);
-    }
-
-    await prisma.file.createMany({
-      data: filesToCreate.map((file) => ({
-        ...file,
-        fileSize: Math.floor(file.fileSize),
-        libraryId: library.id,
-      })),
-    });
-
-    await prisma.file.deleteMany({
-      where: { filePath: { notIn: foundPaths }, libraryId: library.id },
-    });
-
-    await prisma.$disconnect();
-    console.log('Done scanning');
-
-    libraryProber.run();
-  } finally {
-    process.exit();
-  }
-};
